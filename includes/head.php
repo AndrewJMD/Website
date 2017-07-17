76cc9190ea5c8c58baa6c83d63476762d0197f10
rye761 <ryebread761@gmail.com>
2017-07-17 12:24:01 -0600

    Initial SASS and PHP files.

---
+++ includes/head.php
@@ -0,0 +1,6 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>CompCamps</title>
+    <link rel="stylesheet" href="css/style.css">
+  </head>
---
+++ includes/header.php
@@ -0,0 +1,13 @@
+  <body>
+    <header>
+      <img src="img/logo.png">
+      <nav>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Campers</a></li>
+          <li><a href="#">FAQ</a></li>
+          <li><a href="#">Mentors</a></li>
+          <li><a href="#">Register</a></li>
+        </ul>
+      </nav>
+    </header>
