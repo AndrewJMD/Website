ac3bec6a41f1606b36ad49dd603925d04615cecf
rye761 <ryebread761@gmail.com>
2017-07-18 16:31:42 -0600

    Home page banner

--- index.php
+++ index.php
@@ -2,4 +2,10 @@
 $page_title = 'Home';
 include('includes/head.php');
 include('includes/header.php');
-include('includes/footer.php'); ?>
+?>
+
+<!-- Using a background to place the image here. background-* has better support
+and more flexability than object-fit -->
+<a id="home-banner" class="banner" href="register.php"></a>
+
+<?php include('includes/footer.php'); ?>
