cd6bd24e36e96002d7995c59cdfbc9a1df571c04
rye761 <ryebread761@gmail.com>
2017-07-19 20:32:16 -0600

    initial footer created

--- includes/footer.php
+++ includes/footer.php
@@ -1,2 +1,26 @@
+  <footer>
+
+    <?php
+      $sponsors = [
+        // image name, sponsor link
+        ['cslogo.png', 'http://www.cs.uregina.ca/'],
+        ['strategylab.png', 'http://strategylab.ca/'],
+        ['telus.png', 'http://www.telus.com/'],
+        ['innovationplacelogo.png', 'http://innovationplace.com/'],
+        ['capital.png', 'http://capitalautomall.ca/'],
+        ['uofrlogo.png', 'https://www.uregina.ca/'],
+        ['metriclogo.png', 'http://metric-hosting.ca/'],
+        ['isc.png', 'https://www.isc.ca/Pages/default.aspx']
+      ];
+
+      foreach ($sponsors as $sponsor) {
+        $image = 'img/sponsors/'.$sponsor[0];
+        $link = $sponsor[1];
+
+        echo "<a href=\"$link\"><img src=\"$image\"></a>";
+      }
+    ?>
+
+  </footer>
   </body>
 </html>
