f104736c7f9519724fa5200d283c3c512cd59d95
rye761 <ryebread761@gmail.com>
2017-07-20 11:39:48 -0600

    add footer image alts.

--- includes/footer.php
+++ includes/footer.php
@@ -2,22 +2,23 @@
 
     <?php
       $sponsors = [
-        // image name, sponsor link
-        ['cslogo.png', 'http://www.cs.uregina.ca/'],
-        ['strategylab.png', 'http://strategylab.ca/'],
-        ['telus.png', 'http://www.telus.com/'],
-        ['innovationplacelogo.png', 'http://innovationplace.com/'],
-        ['capital.png', 'http://capitalautomall.ca/'],
-        ['uofrlogo.png', 'https://www.uregina.ca/'],
-        ['metriclogo.png', 'http://metric-hosting.ca/'],
-        ['isc.png', 'https://www.isc.ca/Pages/default.aspx']
+        // image name, sponsor link, name
+        ['cslogo.png', 'http://www.cs.uregina.ca/', 'U of R Computer Science'],
+        ['strategylab.png', 'http://strategylab.ca/', 'Strategy Lab'],
+        ['telus.png', 'http://www.telus.com/', 'Telus'],
+        ['innovationplacelogo.png', 'http://innovationplace.com/', 'Innovation Place'],
+        ['capital.png', 'http://capitalautomall.ca/', 'Capital'],
+        ['uofrlogo.png', 'https://www.uregina.ca/', 'University of Regina'],
+        ['metriclogo.png', 'http://metric-hosting.ca/', 'Metric Hosting'],
+        ['isc.png', 'https://www.isc.ca/Pages/default.aspx', 'ISC']
       ];
 
       foreach ($sponsors as $sponsor) {
         $image = 'img/sponsors/'.$sponsor[0];
         $link = $sponsor[1];
+        $alt = $sponsor[2].' Logo';
 
-        echo "<a href=\"$link\"><img src=\"$image\"></a>";
+        echo "<a href=\"$link\"><img src=\"$image\" alt=\"$alt\"></a>";
       }
     ?>
 
