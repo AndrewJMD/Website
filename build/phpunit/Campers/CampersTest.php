08c24946fb8d6f4d19b882c121cc34c61a02dc28
Brett Mayson <brett@bmandesigns.com>
2017-12-20 16:51:14 -0600

    Additional tests

--- build/phpunit/Campers/CampersTest.php
+++ build/phpunit/Campers/CampersTest.php
@@ -12,29 +12,43 @@
   */
   final class CampersTest extends TestCase
   {
+    const EX_DATA = array(
+      "_id" => "100",
+      "name" => "John Doe",
+      "discriminator" => "228",
+      "password" => "1\$salt\$fakepassword",
+      "dob" => "",
+      "health_card" => "ABC123",
+      "phone" => "5555824714",
+      "parent_name" => "Jake Doe",
+      "email" => "jake@icloud.com",
+      "health_notes" => "",
+      "gender" => "Male",
+      "level" => "2",
+      "shirt" => "Adult XL",
+      "change_pass" => "0",
+      "hash_pass" => "1"
+    );
+
     public function testCamperCanBeCreatedFromValidRow(): void
     {
-      $row = array(
-        "_id" => "100",
-        "name" => "John Doe",
-        "discriminator" => "228",
-        "password" => "1\$salt\$fakepassword",
-        "dob" => "",
-        "health_card" => "ABC123",
-        "phone" => "5555824714",
-        "parent_name" => "Jake Doe",
-        "email" => "jake@icloud.com",
-        "health_notes" => "",
-        "gender" => "Male",
-        "level" => "2",
-        "shirt" => "Adult XL",
-        "change_pass" => "0",
-        "hash_pass" => "1"
-      );
       $this->assertInstanceOf(
         Camper::class,
-        new Camper($row)
+        new Camper(CampersTest::EX_DATA)
       );
+    }
+
+    public function testCamperNameCreatedProperly(): void
+    {
+      $camper = new Camper(CampersTest::EX_DATA);
+      $this->expectOutputString("john.228");
+      print $camper->username;
+    }
+
+    public function testCamperSafeName(): void
+    {
+      $this->expectOutputString("fake.123");
+      print Camper::SafeName(array("name" => "Fake Name", "discriminator"=> "123"));
     }
   }
 ?>
