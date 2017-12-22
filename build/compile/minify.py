7930478a237a9390c631a9b4a4783eb66dfbcfb6
Brett Mayson <brett@bmandesigns.com>
2017-12-22 06:27:16 -0600

    Minify Fix Attempt
    The previous attempt ended up with empty js files

--- build/compile/minify.py
+++ build/compile/minify.py
@@ -2,4 +2,5 @@ import glob, os
 
 for filename in glob.iglob('www/**/*.js', recursive=True):
     if not filename.endswith(".min.js"):
-        os.system("uglifyjs \""+filename+"\" > \""+filename+"\"")
+        print("uglifyjs "+filename+" > "+filename)
+        os.system("uglifyjs "+filename+" > "+filename)
