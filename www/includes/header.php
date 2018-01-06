a5b08bdbfc8d19708c31ba06a4917713d448d39b
rye761 <ryebread761@gmail.com>
2018-01-06 17:31:45 -0600

    Basic register form for testing styles

--- www/includes/header.php
+++ www/includes/header.php
@@ -7,7 +7,7 @@
           <li><a href="#">Campers</a></li>
           <li><a href="./faq.php">FAQ</a></li>
           <li><a href="#">Mentors</a></li>
-          <li><a href="#">Register</a></li>
+          <li><a href="./register.php">Register</a></li>
         </ul>
         <div id="nav-handle"><i class="fa fa-bars" aria-hidden="true"></i> Menu</div>
       </nav>
---
+++ www/register.php
@@ -0,0 +1,28 @@
+<?php
+$page_title = 'Register';
+include('includes/head.php');
+include('includes/header.php');
+?>
+
+<h1 class="title">Register</h1>
+
+<section class="register-section">
+  <form class="register-form">
+    <label for="firstname">First Name</label>
+    <input type="text" id="firstname" name="firstname" />
+    <br />
+    <label for="week">Week</label>
+    <select id="week">
+      <option value="1">Week One</option>
+      <option value="2">Week Two</option>
+    </select>
+    <br />
+    <label for="coverletter">Tell Us About Yourself</label><br/>
+    <textarea id="coverletter" rows="6" cols="50">
+    </textarea>
+    <br />
+    <input type="submit" value="Apply" />
+  </form>
+</section>
+
+<?php include('includes/footer.php'); ?>
