6ae5679a2d23eaf3e8690b65d0be918e1c5d32cc
rye761 <ryebread761@gmail.com>
2018-03-27 14:23:36 -0600

    slideshow arrow fix

--- www/includes/footer.php
+++ www/includes/footer.php
@@ -1,7 +1,7 @@
     <footer>
       <div id="slideshow">
-        <i @click="moveLeft" class="slideshow-button fa fa-2x fa-chevron-left"
-          aria-label="Move image slider left"></i>
+        <div @click="moveLeft" class="slideshow-button"><i class="fa fa-2x fa-chevron-left"
+          aria-label="Move image slider left"></i></div>
         <div id="slideshow-window">
           <div id="slideshow-items" :style="{ transform: transformStyle }">
             <div v-for="logo in logos" class="logo-container">
@@ -13,8 +13,8 @@
             </div>
           </div>
         </div>
-        <i @click="moveRight(true)" class="slideshow-button fa fa-2x fa-chevron-right"
-          aria-label="Move image slider right"></i>
+        <div @click="moveRight(true)" class="slideshow-button"><i class="fa fa-2x fa-chevron-right"
+          aria-label="Move image slider right"></i></div>
       </div>
     </footer>
     <script src="js/footerSlideshow.js"></script>
