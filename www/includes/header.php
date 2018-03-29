2d62921448f59c5a3b6673ffd5ae43b7a091bf34
rye761 <ryebread761@gmail.com>
2018-03-28 18:44:30 -0600

    mentors stub page

--- www/includes/header.php
+++ www/includes/header.php
@@ -6,7 +6,7 @@
           <li><a href="./">Home</a></li>
           <li><a href="./campers">Campers</a></li>
           <li><a href="./faq">FAQ</a></li>
-          <li><a href="#">Mentors</a></li>
+          <li><a href="./mentors">Mentors</a></li>
           <li><a href="./register">Register</a></li>
         </ul>
         <div id="nav-handle"><i class="fa fa-bars" aria-hidden="true"></i> Menu</div>
