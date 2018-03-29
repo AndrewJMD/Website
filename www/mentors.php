f02273d8aaef47dd241d2189cb25e3a38cf7bd5f
rye761 <ryebread761@gmail.com>
2018-03-28 18:50:14 -0600

    forgetten mentors file

---
+++ www/mentors.php
@@ -0,0 +1,12 @@
+<?php
+$page_title = 'Mentors';
+include('includes/head.php');
+include('includes/header.php');
+?>
+<h1 class="title">Mentors</h1>
+
+<section class="faq-section">
+  <h1 class="text-center">More information about our mentors coming soon!</h1>
+</section>
+
+<?php include('includes/footer.php'); ?>
