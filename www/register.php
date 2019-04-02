f6278d9a36aa6b5247249938b277a138c6a6f113
Brett <brett@bmandesigns.com>
2019-04-02 00:25:34 -0600

    2019 info

--- www/register.php
+++ www/register.php
@@ -4,13 +4,10 @@ include('includes/head.php');
 include('includes/header.php');
 ?>
 
-<h1 class="title">CompCamps 2018</h1>
+<h1 class="title">CompCamps 2019</h1>
 
 <section class="register-section">
-  <h2>Registration is <b>CLOSED</b></h2>
-  <h3>$350</h3>
-  <h3>First week: <b>CLOSED</b></h3>
-  <h3>Second week: <b>CLOSED</b></h3>
+  <h2>Registration Opening Soon</h2>
 </section>
 
 <?php include('includes/footer.php'); ?>
