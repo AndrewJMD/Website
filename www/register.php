9f1fab1485f4507724bb843cf083fee0c43c142d
Brett <brett@bmandesigns.com>
2019-04-02 15:08:57 -0600

    open registration

--- www/register.php
+++ www/register.php
@@ -7,7 +7,12 @@ include('includes/header.php');
 <h1 class="title">CompCamps 2019</h1>
 
 <section class="register-section">
-  <h2>Registration Opening Soon</h2>
+  <h2>Registration Open</h2>
+  <p>
+    $300 for 1 Week<br/>
+    $500 for 2 Weeks
+  </p>
+  <a class="button" href="dash/register">Register</a>
 </section>
 
 <?php include('includes/footer.php'); ?>
