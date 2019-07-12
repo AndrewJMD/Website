2613822e5a0bbc5c0ab67f0944a4fac2a1b46428
Brett <brett@bmandesigns.com>
2019-07-12 09:49:12 -0600

    2019 update

--- www/mentors.php
+++ www/mentors.php
@@ -8,12 +8,17 @@ include('includes/header.php');
   <div class="mentor-card">
     <img src="img/mentors/paul.jpg" width="150" height="150" />
     <div class="name">Paul Hewitt</div>
-    <div class="position">Coordinator / Photo Editing</div>
+    <div class="position">Coordinator / Machine Learning</div>
   </div>
   <div class="mentor-card">
     <img src="img/mentors/brett.jpg" width="150" height="150" />
     <div class="name">Brett Mayson</div>
-    <div class="position">Director / Python</div>
+    <div class="position">Coordinator / Game Dev</div>
+  </div>
+  <div class="mentor-card">
+    <img src="img/mentors/ryan.jpg" width="150" height="150" />
+    <div class="name">Ryan Aird</div>
+    <div class="position">Director</div>
   </div>
   <div class="mentor-card">
     <img src="img/mentors/cole.jpg" width="150" height="150" />
@@ -24,44 +29,34 @@ include('includes/header.php');
 <section class="mentor-section text-center">
   <h3>Mentors</h3>
   <div class="mentor-card">
-    <img src="img/mentors/landon.jpg" width="150" height="150" />
-    <div class="name">Landon Rohatensky</div>
-    <div class="position">Drones</div>
-  </div>
-  <div class="mentor-card">
     <img src="img/mentors/kim.jpg" width="150" height="150" />
     <div class="name">Kim Nguyen</div>
-    <div class="position">Video Editing</div>
+    <div class="position">&nbsp;</div>
   </div>
   <div class="mentor-card">
     <img src="img/mentors/kyle.jpg" width="150" height="150" />
     <div class="name">Kyle Smith</div>
-    <div class="position">Crypto</div>
+    <div class="position">&nbsp;</div>
   </div>
   <div class="mentor-card">
     <img src="img/mentors/connor.jpg" width="150" height="150" />
     <div class="name">Connor Brezinsky</div>
-    <div class="position">Web Dev</div>
+    <div class="position">&nbsp;</div>
   </div>
   <div class="mentor-card">
     <img src="img/mentors/david.jpg" width="150" height="150" />
     <div class="name">David Crossman</div>
-    <div class="position">Web Dev</div>
-  </div>
-  <div class="mentor-card">
-    <img src="img/mentors/noimage.png" width="150" height="150" />
-    <div class="name">Dylan Rush</div>
-    <div class="position">3D Printing</div>
+    <div class="position">&nbsp;</div>
   </div>
   <div class="mentor-card">
     <img src="img/mentors/taylor.png" width="150" height="150" />
     <div class="name">Taylor Petrychyn</div>
-    <div class="position">&nbsp;</div>
+    <div class="position">Crypto</div>
   </div>
   <div class="mentor-card">
     <img src="img/mentors/lucas.jpg" width="150" height="150" />
     <div class="name">Lucas Kuffner</div>
-    <div class="position">&nbsp;</div>
+    <div class="position">Wireless Technology</div>
   </div>
   <div class="mentor-card">
     <img src="img/mentors/nelson.jpg" width="150" height="150" />
@@ -69,37 +64,17 @@ include('includes/header.php');
     <div class="position">&nbsp;</div>
   </div>
   <div class="mentor-card">
-    <img src="img/mentors/jason.jpg" width="150" height="150" />
-    <div class="name">Jason Wist</div>
-    <div class="position">&nbsp;</div>
-  </div>
-  <div class="mentor-card">
-    <img src="img/mentors/ryan.jpg" width="150" height="150" />
-    <div class="name">Ryan Aird</div>
+    <img src="img/mentors/bennett.jpg" width="150" height="150" />
+    <div class="name">Bennett Eidsness</div>
     <div class="position">&nbsp;</div>
   </div>
 </section>
 <section class="mentor-section text-center">
   <h3>Mentors-in-Training</h3>
   <div class="mentor-card">
-    <img src="img/mentors/bennett.jpg" width="150" height="150" />
-    <div class="name">Bennett Eidsness</div>
-    <div class="position">&nbsp;</div>
-  </div>
-  <div class="mentor-card">
     <img src="img/mentors/kaitlin.jpg" width="150" height="150" />
     <div class="name">Kaitlin Zastrow</div>
-    <div class="position">&nbsp;</div>
-  </div>
-  <div class="mentor-card">
-    <img src="img/mentors/rhiannon.jpg" width="150" height="150" />
-    <div class="name">Rhiannon Ridell</div>
-    <div class="position">3D Modeling</div>
-  </div>
-  <div class="mentor-card">
-    <img src="img/mentors/travis.jpg" width="150" height="150" />
-    <div class="name">Travis Brady</div>
-    <div class="position">&nbsp;</div>
+    <div class="position">Web Dev</div>
   </div>
   <div class="mentor-card">
     <img src="img/mentors/austin.jpg" width="150" height="150" />
